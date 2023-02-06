a623f189966f52c8b9fd0e6a412194d1795ad1d5
274tibjeuw6 <115452104+274tibjeuw6@users.noreply.github.com>
2023-02-06 15:44:03 +0800

    Add files via upload

---
+++ src for Non-SPJ extension/include/parser/query_split.h
@@ -0,0 +1,97 @@
+/*-------------------------------------------------------------------------
+ *
+ *
+ *
+ *
+ * IDENTIFICATION
+ *	  src/include/parser/query_split.c
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef QUERY_SPLIT_H
+#define QUERY_SPLIT_H
+
+#include "postgres.h"
+
+#include <fcntl.h>
+#include <limits.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#ifdef HAVE_SYS_SELECT_H
+#include <sys/select.h>
+#endif
+#ifdef HAVE_SYS_RESOURCE_H
+#include <sys/time.h>
+#include <sys/resource.h>
+#endif
+#ifndef HAVE_GETRUSAGE
+#include "rusagestub.h"
+#endif
+#include "access/parallel.h"
+#include "access/printtup.h"
+#include "access/table.h"
+#include "access/xact.h"
+#include "catalog/pg_type.h"
+#include "catalog/namespace.h"
+#include "commands/async.h"
+#include "commands/createas.h"
+#include "commands/matview.h"
+#include "commands/prepare.h"
+#include "executor/spi.h"
+#include "jit/jit.h"
+#include "libpq/libpq.h"
+#include "libpq/pqformat.h"
+#include "libpq/pqsignal.h"
+#include "miscadmin.h"
+#include "nodes/parsenodes.h"
+#include "nodes/pathnodes.h"
+#include "nodes/pg_list.h"
+#include "nodes/print.h"
+#include "nodes/makefuncs.h"
+#include "optimizer/optimizer.h"
+#include "pgstat.h"
+#include "pg_trace.h"
+#include "parser/analyze.h"
+#include "parser/parser.h"
+#include "pg_getopt.h"
+#include "postmaster/autovacuum.h"
+#include "postmaster/postmaster.h"
+#include "replication/logicallauncher.h"
+#include "replication/logicalworker.h"
+#include "replication/slot.h"
+#include "replication/walsender.h"
+#include "rewrite/rewriteHandler.h"
+#include "storage/bufmgr.h"
+#include "storage/ipc.h"
+#include "storage/proc.h"
+#include "storage/procsignal.h"
+#include "storage/sinval.h"
+#include "tcop/fastpath.h"
+#include "tcop/pquery.h"
+#include "tcop/tcopprot.h"
+#include "tcop/utility.h"
+#include "utils/lsyscache.h"
+#include "utils/memutils.h"
+#include "utils/ps_status.h"
+#include "utils/rel.h"
+#include "utils/snapmgr.h"
+#include "utils/timeout.h"
+#include "utils/timestamp.h"
+#include "mb/pg_wchar.h"
+#include "utils/builtins.h"
+
+/*
+typedef struct
+{
+	char* relname;
+	Bitmapset* bms;
+}My;
+*/
+
+void doQSparse(const char* query_string, const char* commandTag, Query* querytree, char* completionTag, CommandDest whereDest);
+
+//double Gaussian(double mu, double sigma);
+//void writeFile(char* file, QueryDesc* queryDesc);
+
+#endif
